72a0b37c2b3437d6d55e65f57212b92c5894ecef
shahinexy <shahinexy@gmail.com>
2024-06-10 09:05:00 +0600

    sales repot page xl fil download function completed

--- src/Dashboard/Admin/SalesReport.jsx
+++ src/Dashboard/Admin/SalesReport.jsx
@@ -3,20 +3,25 @@ import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 import Loader from "../../components/Loader";
 import { GoDotFill } from "react-icons/go";
+import { useRef } from "react";
+import { DownloadTableExcel } from "react-export-table-to-excel";
+import { FaFileDownload } from "react-icons/fa";
 
 const SalesReport = () => {
-    const axiosSecure = useAxiosSecure();
+  const axiosSecure = useAxiosSecure();
 
-    const { data, isPending, isError, error, refetch } = useQuery({
-      queryKey: ["payments"],
-      queryFn: async () => {
-        const res = await axiosSecure.get(`/payments`);
-        return res.data;
-      },
-    });
-  
-    if (isPending) return <Loader></Loader>;
-    if (isError) console.log(error.message);
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["payments"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/payments`);
+      return res.data;
+    },
+  });
+
+  const tableRef = useRef(null);
+
+  if (isPending) return <Loader></Loader>;
+  if (isError) console.log(error.message);
   return (
     <div>
       <Helmet>
@@ -24,10 +29,21 @@ const SalesReport = () => {
       </Helmet>
       <div className="flex justify-between bg-secondary py-2 px-2 text-white items-center">
         <p className="text-xl font-semibold ">Payment Management</p>
+
+        <DownloadTableExcel
+        filename="Sales Report"
+        sheet="Salse"
+        currentTableRef={tableRef.current}
+      >
+        <button className="flex sm:gap-2 items-center bg-primary py-2 sm:px-5 px-2 rounded-none hover:bg-[#44adb0] hover:scale-95 duration-300"> Export excel <FaFileDownload /> </button>
+      </DownloadTableExcel>
       </div>
 
       <div className="mt-6 overflow-x-auto">
-        <table className="w-full p-6 text-left whitespace-nowrap">
+        <table
+          ref={tableRef}
+          className="w-full p-6 text-left whitespace-nowrap"
+        >
           <thead>
             <tr className="text-left bg-secondary/70 text-white">
               <th className="p-3">No.</th>
@@ -41,36 +57,43 @@ const SalesReport = () => {
               <th className="p-3">Status</th>
             </tr>
           </thead>
-            {data?.map((paymentData, idx) => (
-              <tbody key={paymentData._id}>
-                {
-                  paymentData.medicineDatas.map(medicine =>               <tr
-                    key={medicine.id}
-                    className="bg-secondary/10 border-b border-secondary/30 hover:bg-secondary/30"
+          {data?.map((paymentData, idx) => (
+            <tbody key={paymentData._id}>
+              {paymentData.medicineDatas.map((medicine) => (
+                <tr
+                  key={medicine.id}
+                  className="bg-secondary/10 border-b border-secondary/30 hover:bg-secondary/30"
+                >
+                  <td className="px-3 py-2 pl-4">{idx + 1}.</td>
+                  <td className="px-3 py-2 font-medium">
+                    {medicine.medicienName}
+                  </td>
+                  <td className="px-3 py-2">{medicine.buyerEmail}</td>
+                  <td className="px-3 py-2">{medicine.userEmail}</td>
+                  <td className="px-3 py-2">{paymentData.transitionId}</td>
+                  <td className="px-3 py-2">{medicine.quantity}</td>
+                  <td className="px-3 py-2">
+                    {medicine.discount > 0
+                      ? medicine.quantity * medicine.discountPrice
+                      : medicine.quantity * medicine.price}
+                    $
+                  </td>
+                  <td className="px-3 py-2">
+                    {paymentData.date.split("T")[0]}
+                  </td>
+                  <td
+                    className={`px-3 py-2 flex gap-1 items-center ${
+                      paymentData.status === "pending"
+                        ? "text-orange-500"
+                        : "text-green-500"
+                    }`}
                   >
-                    <td className="px-3 py-2 pl-4">{idx + 1}.</td>
-                    <td className="px-3 py-2 font-medium">
-                      {medicine.medicienName}
-                    </td>
-                    <td className="px-3 py-2">{medicine.buyerEmail}</td>
-                    <td className="px-3 py-2">{medicine.userEmail}</td>
-                    <td className="px-3 py-2">{paymentData.transitionId}</td>
-                    <td className="px-3 py-2">{medicine.quantity}</td>
-                    <td className="px-3 py-2">{medicine.discount > 0 ? medicine.quantity * medicine.discountPrice : medicine.quantity * medicine.price}$</td>
-                    <td className="px-3 py-2">{paymentData.date.split('T')[0]}</td>
-                    <td
-                      className={`px-3 py-2 flex gap-1 items-center ${
-                        paymentData.status === "pending"
-                          ? "text-orange-500"
-                          : "text-green-500"
-                      }`}
-                    >
-                      {paymentData.status} <GoDotFill className="text-lg" />{" "}
-                    </td>
-                  </tr>)
-                }
-              </tbody>
-            ))}
+                    {paymentData.status} <GoDotFill className="text-lg" />{" "}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          ))}
         </table>
       </div>
     </div>
---
+++ src/pages/InvoicePage/InvoicePage.jsx
@@ -0,0 +1,10 @@
+
+const InvoicePage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default InvoicePage;
